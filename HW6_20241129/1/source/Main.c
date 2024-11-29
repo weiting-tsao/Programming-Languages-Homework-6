5278aea22f29573f90a9151de7552c56120e27b5
weiting.tsao <t110362504@ntut.org.tw>
2024-11-29 17:23:12 +0800

    first

---
+++ HW6_20241129/1/source/Main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+// 定義結構 card
+struct card {
+    char* face; // 定義點數
+    char* suit; // 定義花色
+};
+
+int main(void) {
+    struct card aCard;       // 宣告一個 struct card 變數
+    struct card* cardPtr;    // 宣告一個指向 struct card 的指標
+
+    aCard.face = "Ace";
+    aCard.suit = "Spades";
+
+    cardPtr = &aCard; // 將 aCard 的地址賦值給 cardPtr
+
+    // 使用結構成員運算子和指標運算子輸出結果
+    printf("%s%s%s\n%s%s%s\n%s%s%s\n", aCard.face, " of ", aCard.suit,  // 使用結構變數直接訪問
+       cardPtr->face, " of ", cardPtr->suit, // 使用指標訪問
+       (*cardPtr).face, " of ", (*cardPtr).suit); // 另一種指標訪問方式
+
+    return 0;
+}
---
+++ HW6_20241129/2/source/Main.c
@@ -0,0 +1,66 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* 定義Card結構 */
+struct card {
+    const char* face; /* 定義指標face */
+    const char* suit; /* 定義指標suit */
+}; /* 結構結束 */
+
+typedef struct card Card; /* 定義新類型名稱 */
+
+/* 函數原型 */
+void fillDeck(Card* const wDeck, const char* wFace[], const char* wSuit[]);
+void shuffle(Card* const wDeck);
+void deal(const Card* const wDeck);
+
+int main(void) {
+    Card deck[52]; /* 定義一副牌的陣列 */
+
+    /* 初始化點數的指標陣列 */
+    const char* face[] = {
+        "Ace", "Deuce", "Three", "Four", "Five",
+        "Six", "Seven", "Eight", "Nine", "Ten",
+        "Jack", "Queen", "King"
+    };
+
+    /* 初始化花色的指標陣列 */
+    const char* suit[] = { "Hearts", "Diamonds", "Clubs", "Spades" };
+
+    srand(time(NULL)); /* 設定隨機數生成器的種子 */
+
+    fillDeck(deck, face, suit); /* 填充牌組 */
+    shuffle(deck);             /* 洗牌 */
+    deal(deck);                /* 發牌 */
+
+    return 0; /* 表示程式順利結束 */
+}
+
+/* 將字串放入Card結構中 */
+void fillDeck(Card* const wDeck, const char* wFace[], const char* wSuit[]) {
+    for (int i = 0; i < 52; i++) {
+        wDeck[i].face = wFace[i % 13];
+        wDeck[i].suit = wSuit[i / 13];
+    }
+}
+
+/* 洗牌函數 */
+void shuffle(Card* const wDeck) {
+    for (int i = 0; i < 52; i++) {
+        int j = rand() % 52; /* 生成0到51之間的隨機數 */
+        Card temp = wDeck[i];
+        wDeck[i] = wDeck[j];
+        wDeck[j] = temp;
+    }
+}
+
+/* 發牌函數 */
+void deal(const Card* const wDeck) {
+    for (int i = 0; i < 52; i++) {
+        printf("%5s of %-8s%s",
+            wDeck[i].face,
+            wDeck[i].suit,
+            (i + 1) % 4 ? " " : "\n");
+    }
+}
